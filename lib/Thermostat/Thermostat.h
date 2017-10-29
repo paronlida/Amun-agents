1b4865e357e992c1fe80f54353341f54d916dc7f
Aitch0R <31582708+Aitch0R@users.noreply.github.com>
2017-10-29 08:51:11 +0100

    Update Thermostat.h

--- lib/Thermostat/Thermostat.h
+++ lib/Thermostat/Thermostat.h
@@ -1,27 +1,53 @@
-class Relay {
+class Thermostat {
 public:
 
-int pin;
+int pin1;
+int pin2
 String stat;
 String address;
 String cmdid="0";
 
-Relay (int pin1){
-	pin = pin1;
-	pinMode(pin,OUTPUT);
+Relay (int pin_1, int pin_2){
+	pin1 = pin_1;
+	pin2 = pin_2;
+	pinMode(pin1,OUTPUT);
+	pinMode(pin1,OUTPUT);
 	digitalWrite(pin,HIGH);
 	stat="0";
 	address=String(pin)+",";
 }
-
+	
+void turnon(){
+	for (i=0, i<30, i++){
+		digitalWrite(pin1,LOW);
+		delay(10);
+		digitalWrite(pin2,LOW);
+		delay(10);
+		digitalWrite(pin1,High);
+		delay(10);
+		digitalWrite(pin2,High);
+	}
+}
+void turnoff(){
+	for (i=0, i<30, i++){
+		digitalWrite(pin2,LOW);
+		delay(10);
+		digitalWrite(pin1,LOW);
+		delay(10);
+		digitalWrite(pin2,High);
+		delay(10);
+		digitalWrite(pin1,High);
+	}
+}
+	
 void process (String incoming[]){
 	
 	cmdid=incoming[1];
 	stat=incoming[2];
 	if (stat.equals("1")){
-		digitalWrite(pin,LOW);
+		turnon();
 	}else if (stat.equals("0")){
-		digitalWrite(pin,HIGH);
+		turnoff();
 	}
 	status();
 }
